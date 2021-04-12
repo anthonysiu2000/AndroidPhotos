5941af0ec3b234928be074c4dd7fcf719ed6c1ba
anthonysiu2000 <tonysiu123@yahoo.com>
2021-04-12 00:30:29 -0400

    added connection

--- src/model/Connection.java
+++ src/model/Connection.java
@@ -1,5 +1,20 @@
 package model;
 
 public class Connection {
-
+	//fields
+	private String photoPath;
+	private String aName;
+	//constructor
+	public Connection(String path, String album) {
+		this.photoPath = path;
+		this.aName = album;
+	}
+	//gets photo path
+	public String getPath() {
+		return photoPath;
+	}
+	//gets album name
+	public String getAlbum() {
+		return aName;
+	}
 }
